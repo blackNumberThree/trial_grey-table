f64496942313ecb6d00aee112aee15f6476732e2
blackNumberThree <46719141+blackNumberThree@users.noreply.github.com>
2019-09-28 16:03:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let floatBox = $('#float-box');  
+let widthBox = $('#width-box');
+let crutchBox = $('#crutch-box'); 
+let text =$('#floatBox p');
+
+//little adaptability
+
+floatBox.width(widthBox.width())
+
+if(parseInt(window.innerWidth)<1000)
+{
+	crutchBox.removeClass("fixed"),
+	floatBox.addClass("relative"), 
+	text.css("margin-left", "37%"),
+	text.css("text-align", "center");
+}
+else 
+{
+	text.css("margin-left", "7%"),
+	//toggle float-box position
+	$(window).on('scroll', function () {
+		console.log(crutchBox.offset().top)
+		if( crutchBox.offset().top<709 ) 
+				{ floatBox.removeClass("absolute")}
+			 else 
+			 	{floatBox.addClass("absolute") }
+		if( crutchBox.offset().top>709 ) 
+				{ floatBox.removeClass("fixed")}
+			 else 
+			 	{floatBox.addClass("fixed")  }
+	})
+};
+
+
+
